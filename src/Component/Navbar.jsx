e766e8a7e39fff84bcf1d946ecacf066f4f8eb31
Arju-Hasan <rajuahmeed698@gmail.com>
2025-09-30 11:10:31 +0600

    add a navbar item menu

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+
+
+const items = [
+  { id: 1, name: "Home", urlPath: "/home" },
+  { id: 2, name: "About", urlPath: "/about" },
+  { id: 3, name: "Products", urlPath: "/products" },
+  { id: 4, name: "Blog", urlPath: "/blog" },
+  { id: 5, name: "Contact", urlPath: "/contact" },
+];
+
+
+
+
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Navbar;
